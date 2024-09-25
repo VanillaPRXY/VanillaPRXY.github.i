8f34fd1c2bfe7784fcf01e7408f135808858190a
VanillaPRXY <exotikios@gmail.com>
2024-09-25 09:45:11 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function displayInput() {
+    const userInput = document.getElementById("userInput").value;
+    const outputDiv = document.getElementById("output");
+    outputDiv.textContent = userInput;
+}
